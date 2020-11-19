9959ede1c8a0a9f7eae2556be599a0db75c97eb9
RadoslavK <RadoslavK@kentico.com>
2020-11-19 15:33:03 +0100

    Add dummy protection

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Navigation } from '../features/navigation/components/Navigation';
 import { Routes } from '../features/navigation/components/Routes';
 import {  BrowserRouter as Router } from 'react-router-dom'
 import { Copyright } from './Copyright';
 
 export const App: React.FC = () => {
+  const [pass, setPass] = useState('');
+  const [isLogged, setIsLogged] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  if (!isLogged) {
+    return (
+      <input
+        ref={inputRef}
+        value={pass}
+        onChange={e => setPass(e.currentTarget.value)}
+        onKeyDown={e => {
+          if (e.key === 'Enter' && pass === 'Speedas11') {
+            setIsLogged(true);
+          }
+        }}
+      />
+    )
+  }
+
   return (
     <Router>
       <Copyright />
