0a0c29bb87fff6b6295b7aa03da77c42f13c270d
RadoslavK <RadoslavK@kentico.com>
2020-11-28 09:35:52 +0100

    remove authentication

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,33 +1,10 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { Navigation } from '../features/navigation/components/Navigation';
 import { Routes } from '../features/navigation/components/Routes';
 import {  BrowserRouter as Router } from 'react-router-dom'
 import { Copyright } from './Copyright';
 
 export const App: React.FC = () => {
-  const [pass, setPass] = useState('');
-  const [isLogged, setIsLogged] = useState(false);
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
-
-  if (!isLogged) {
-    return (
-      <input
-        ref={inputRef}
-        value={pass}
-        onChange={e => setPass(e.currentTarget.value)}
-        onKeyDown={e => {
-          if (e.key === 'Enter' && pass === 'Speedas11') {
-            setIsLogged(true);
-          }
-        }}
-      />
-    )
-  }
-
   return (
     <Router>
       <Copyright />
